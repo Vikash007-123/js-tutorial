29ac5ffe92176f328001ee5ab6034fd11053716d
Vikash Kumar <58529569+Vikash007-123@users.noreply.github.com>
2024-07-03 16:57:04 +0000

    assignment

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,26 +1,62 @@
-let score="vikash";
+// let score="vikash";
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let valueInNumber=Number(score)
+// let valueInNumber=Number(score)
 
-console.log(typeof valueInNumber+" "+valueInNumber)
+// console.log(typeof valueInNumber+" "+valueInNumber)
 
-// string 33abc->NaN
-//null-> 0
-//true ->1 or false 0
-//string ->"33"->Number
+// // string 33abc->NaN
+// //null-> 0
+// //true ->1 or false 0
+// //string ->"33"->Number
 
-let isLoggedIn="vikash"
+// let isLoggedIn="vikash"
 
-let booleanIsLogggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLogggedIn)
+// let booleanIsLogggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLogggedIn)
 
-//1 ->true 0->false
-//""->false
-//"vikash"->true
+// //1 ->true 0->false
+// //""->false
+// //"vikash"->true
 
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+// //************************************* Operations ***********************************/
+// let value=3
+// let negValue=-value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2**3)
+// console.log(4/2)
+// console.log(4%3)
+
+// let str1="Hello"
+// let str2=" vikash"
+// let str3=str1+str2
+// console.log(str3)
+// console.log("1"+2) //12
+// console.log(1+"2")  //12
+// console.log("1"+2+2)  //122
+// console.log(1+2+"2") //32
+
+// console.log(+true)
+// console.log(+"")
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)
+//link to study. :- https://tc39.es/ecma262/#sec-ecmascript-data-types-and-values
+
+/*
+1.Assignment rule
+read about postfix and prefix
+
+
+*/
