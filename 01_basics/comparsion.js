5b23068814a43f5f3969895525ac771cd399213d
Vikash Kumar <58529569+Vikash007-123@users.noreply.github.com>
2024-07-03 17:09:21 +0000

    conversion commit

---
+++ 01_basics/comparsion.js
@@ -0,0 +1,17 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(3>=2)
+// console.log(2!=2)
+// console.log(2==2)
+
+// console.log("2" >1)
+// console.log("02">1)
+//Please avoid this type of conversion
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(undefined >0)
+// console.log(undefined ==0)
+// console.log(undefined >=0)
+
+// ====, check the value with strict datatype
