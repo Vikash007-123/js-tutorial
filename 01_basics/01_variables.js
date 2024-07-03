5b24965e39b9256489281b2ae6a095e633529411
Vikash Kumar <58529569+Vikash007-123@users.noreply.github.com>
2024-07-03 15:54:09 +0000

    upload var and datatype file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553;
+let accountEmail='vikash123Gmail.com';
+var accountPwd='12345';
+accountCity='Nalanda';
+let accountState;
+//accountId=23456; //it is not allowed
+console.log(accountId);
+accountEmail="vikashprogrammer123@gmail.com";
+accountPwd="34567"
+accountCity="bangalore"
+/*
+Note:-preferred not to use var, because of issues in block scope or functional scope
+*/
+console.table([accountEmail,accountPwd,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" ; //treat all js code as newer version
+
+//alert(3+3). // we are using node js not browser
+
+console.log(3+
+    3) //code readability should be high
+console.log('Hitesh')
+
+let name="vikash"
+let age=29
+let isLoggedIn=true
+let state=null;
+
+//number range  2to power of 53
+//bigInt
+//String
+//boolean
+//null => standalone value-> typeof as null
+//undefined. // type of undefined
+//symbol => unique
+
+//object
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
