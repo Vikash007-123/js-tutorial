a26a404121cc7f03bb93bd9172678e9b66460ffd
Vikash Kumar <58529569+Vikash007-123@users.noreply.github.com>
2024-07-03 16:38:42 +0000

    conversion of datatype

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,26 @@
+let score="vikash";
+
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber+" "+valueInNumber)
+
+// string 33abc->NaN
+//null-> 0
+//true ->1 or false 0
+//string ->"33"->Number
+
+let isLoggedIn="vikash"
+
+let booleanIsLogggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLogggedIn)
+
+//1 ->true 0->false
+//""->false
+//"vikash"->true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
